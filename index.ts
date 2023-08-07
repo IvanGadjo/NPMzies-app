4e2a832384054317b515c8b43081878aa4e29de9
IvanGadjo <ivan.gadzovski@gmail.com>
2023-08-07 15:48:14 +0200

    update: test database insertion

--- index.ts
+++ index.ts
@@ -1,9 +1,4 @@
-import { db } from "./database/connection";
 import { app } from "./server";
-
-// app.listen(3002, () => {
-//   console.log("NPMzies started on port 3002");
-// });
 
 (async () => {
   try {
@@ -11,6 +6,6 @@ import { app } from "./server";
       console.log("NPMzies started on port 3002");
     });
   } catch (error) {
-    console.error("Failed to connect to the database:", error);
+    console.error("Failed to start the app: ", error);
   }
 })();
--- server.ts
+++ server.ts
@@ -1,7 +1,20 @@
 import express, { Express, Request, Response } from "express";
+import { InferModel } from "drizzle-orm";
+import { db } from "./database/connection";
+import * as schema from "./database/schema";
 
 export const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
+  // * create a new test project
+  const testProject: InferModel<typeof schema.projects, "insert"> = {
+    name: "test project",
+    description: "test description",
+  };
+  await db.insert(schema.projects).values(testProject).returning();
+
+  const projects = await db.select().from(schema.projects);
+  console.log(projects);
+
   res.send("Hello World!");
 });
